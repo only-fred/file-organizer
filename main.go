26db84eecf388a109d3641d8f5dc9ee41a0af711
only-fred <rochaeduardo997@protonmail.com>
2020-06-22 10:35:34 -0300

    Solved problem that directory was generated with a space at the end, directory's name is now in Uppercase and function to create the directory has been created

--- main.go
+++ main.go
@@ -5,10 +5,12 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 func main() {
-	var _fileExtension string
+	var fileExtension string
+	var fileName string
 
 	files, err := ioutil.ReadDir(".")
 	if err != nil {
@@ -16,34 +18,36 @@ func main() {
 	}
 
 	for _, file := range files {
-		_fileExtension = filepath.Ext(file.Name())
+		fileName = file.Name()
+		_fileExtension := filepath.Ext(fileName)
 
-		_runeFileExtension := []rune(_fileExtension)
+		runeFileExtension := []rune(_fileExtension)
 
-		for i := 1; i < len(_runeFileExtension); i++ {
-			_runeFileExtension[(i - 1)] = _runeFileExtension[i]
+		fileExtension = ""
 
-			if i == (len(_runeFileExtension) - 1) {
-				_runeFileExtension[i] = ' '
-			}
+		for i := 1; i < len(runeFileExtension); i++ {
+			fileExtension = fileExtension + string(runeFileExtension[i])
 		}
 
-		_fileExtension = string(_runeFileExtension)
+		fileExtension = strings.ToUpper(fileExtension)
 
-		if _, err := os.Stat(_fileExtension); os.IsNotExist(err) {
-			os.Mkdir(_fileExtension, os.ModePerm)
-		}
+		createNewDirectory(fileExtension)
 
 		oldLocation := file.Name()
-		newLocation := _fileExtension + "/" + file.Name()
+		newLocation := fileExtension + "/" + file.Name()
+		isDirectory := file.IsDir()
 
-		testDirectory := file.IsDir()
-
-		if testDirectory == false && _fileExtension != "go " {
+		if isDirectory == false {
 			err := os.Rename(oldLocation, newLocation)
 			if err != nil {
 				log.Fatal(err)
 			}
 		}
+	}
+}
+
+func createNewDirectory(fileExtension string) {
+	if _, err := os.Stat(fileExtension); os.IsNotExist(err) {
+		os.Mkdir(fileExtension, os.ModePerm)
 	}
 }
